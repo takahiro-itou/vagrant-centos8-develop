d14c23021203a549019287453e50b77b44fe0182
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-08-14 23:29:13 +0900

    仮想マシンに RAM Disk を設定

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -9,4 +9,11 @@ sudo  timedatectl  set-timezone Asia/Tokyo
 sudo  yum  install -y  \
     screen
 
+# RamDisk
+sudo  mkdir        /ramdisk
+sudo  chmod  1777  /ramdisk
+echo  -e  "tmpfs\t/ramdisk\ttmpfs\trw,size=2048m,x-gvfs-show\t0\t0"  \
+      |  sudo  tee -a  /etc/fstab
+sudo  mount  -a
+
 date  >  /root/.provision.root
